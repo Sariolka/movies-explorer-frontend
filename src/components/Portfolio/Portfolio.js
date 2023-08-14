53012db83a7e88b288b713c696b0624b79afc203
Sariolka <sariola@list.ru>
2023-08-15 00:06:13 +0300

    делать Main

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,6 +1,46 @@
 import React from "react";
+import "./Portfolio.css";
 
 function Portfolio() {
-    return
+  return (
+    <section className="portfolio">
+      <h2 className="portfolio__title">Портфолио</h2>
+      <ul className="portfolio__list">
+        <li className="portfolio__item">
+          <a
+            className="portfolio__link"
+            href="https://github.com/Sariolka/how-to-learn"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Статичный сайт
+          </a>
+          <span>↗</span>
+        </li>
+        <li className="portfolio__item">
+          <a
+            className="portfolio__link"
+            href="https://github.com/Sariolka/russian-travel"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Адаптивный сайт
+          </a>
+          <span>↗</span>
+        </li>
+        <li className="portfolio__item">
+          <a
+            className="portfolio__link"
+            href="https://github.com/Sariolka/react-mesto-api-full-gha"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Одностраничное приложение
+          </a>
+          <span>↗</span>
+        </li>
+      </ul>
+    </section>
+  );
 }
 export default Portfolio;
