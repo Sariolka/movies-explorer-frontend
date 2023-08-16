18c6be1b0ea38452d31d891f088f000127fc50b8
Sariolka <sariola@list.ru>
2023-08-16 14:59:39 +0300

    404

--- src/components/PageNotFound/PageNotFound.js
+++ src/components/PageNotFound/PageNotFound.js
@@ -8,7 +8,6 @@ function PageNotFound() {
       <h2 className="page-notfound__title">404</h2>
       <p className="page-notfound__info">Страница не найдена</p>
       <Link className="page-notfound__link" to="/">
-        {" "}
         Назад
       </Link>
     </section>
